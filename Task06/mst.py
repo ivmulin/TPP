3a4348aac1f990c707fb180766f18866b7f9206d
ivmulin <ivankarpov951@gmail.com>
2023-12-14 09:24:41 +0300

    Pylinted files

--- Task06/mst.py
+++ Task06/mst.py
@@ -1,106 +1,120 @@
+""" Module contains Prim's and Kruskal algorithms """
+
+
 class Queue:
+    """ Queue """
+
     def __init__(self, size, s=0):
+        """ Set up queue """
         self.queue = list(range(size))
         self.keys = [float("inf")] * size
         self.tracked = []
         self.keys[s] = 0
+        self._n: int
 
-    def ExtractMin(self):
-        global iteration
+    def extract_min(self):
+        """ Extracts minimal key """
         m = float("inf")
         i = 0
-        for v in range(len(self.queue)):
-            if self.keys[v] < m and \
-                    self.queue[v] not in self.tracked:
-                m = self.keys[v]
-                i = v
+        for index, elem in enumerate(self.queue):
+            #       for v in range(len(self.queue)):
+            if self.keys[index] < m and \
+                    elem not in self.tracked:
+                m = self.keys[index]
+                i = index
         q = self.queue[i]
         self.tracked.append(q)
         return q
 
-    def isEmpty(self):
+    def is_empty(self):
+        """ Tells if Queue is empty """
         return len(self.tracked) == len(self.queue)
 
     def __iter__(self):
+        """ Queue's iterator """
         self._n = 0
         return self
 
     def __next__(self):
+        """ Queue.__iter__ brother """
         if self._n < len(self.queue):
             self._n += 1
             return self.queue[self._n-1]
         raise StopIteration
 
 
-def Prim(G, s=0):
-    size = len(G)
+def prim(graph, s=0):
+    """ Prim's algorithm """
+    size = len(graph)
     tails = [None] * size
     tails[s] = s
 
     queue = Queue(size, s)
     tree = []
 
-    v = queue.ExtractMin()
-    while not queue.isEmpty():
-        for u in G[v]:
-            if u in queue and queue.keys[u] > G[v][u]:
-                queue.keys[u] = G[v][u]
+    v = queue.extract_min()
+    while not queue.is_empty():
+        for u in graph[v]:
+            if u in queue and queue.keys[u] > graph[v][u]:
+                queue.keys[u] = graph[v][u]
                 tails[u] = v
-        v = queue.ExtractMin()
+        v = queue.extract_min()
         tree.append((tails[v], v))
 
     return tree
 
 
-def FindShortestEdge(G, connected):
-    minpath, edge = float("inf"), list()
-    for e in range(len(G)):
-        for k, v in G[e].items():
-            pair = sorted([e, k])
+def finnd_shortest_edge(graph, connected):
+    """ Finds shortest edge if it's not in connected """
+    minpath, edge = float("inf"), []
+    for vertex, elem in enumerate(graph):
+        for k, v in elem.items():
+            pair = sorted([vertex, k])
             if v < minpath and pair not in connected:
                 edge, minpath = pair, v
     return edge, minpath
 
 
-def AmountOfEdges(G):
+def amount_of_edges(graph):
+    """ Counts amount of edges of a graph """
     a = 0
-    for i in G:
+    for i in graph:
         a += len(i)
     return a // 2  # каждое ребро посчитано дважды
 
 
-def Kruskal(G):
-    size = len(G)
+def kruskal(graph):
+    """ Kruskal's algorithm """
+    size = len(graph)
     forest = []
-    connectedVertices = set()
-    connectedEdges = []
-    isolatedEdges = {}
-    edges = AmountOfEdges(G)
+    connected_vertices = set()
+    isolated_edges = {}
+    edges = amount_of_edges(graph)
 
     for i in range(edges):
-        edge, _ = FindShortestEdge(G, forest)
-        if edge[0] in connectedVertices and edge[1] in connectedVertices:
+        edge, _ = finnd_shortest_edge(graph, forest)
+        if edge[0] in connected_vertices and edge[1] in connected_vertices:
             continue
-        if edge[0] not in connectedVertices and edge[1] not in connectedVertices:
-            isolatedEdges[edge[0]] = isolatedEdges[edge[1]] = edge.copy()
+        if edge[0] not in connected_vertices and edge[1] not in connected_vertices:
+            isolated_edges[edge[0]] = isolated_edges[edge[1]] = edge.copy()
         else:
-            if not isolatedEdges.get(edge[0]):
-                isolatedEdges[edge[1]].append(edge[0])
-                isolatedEdges[edge[0]] = isolatedEdges[edge[1]]
+            if not isolated_edges.get(edge[0]):
+                isolated_edges[edge[1]].append(edge[0])
+                isolated_edges[edge[0]] = isolated_edges[edge[1]]
             else:
-                isolatedEdges[edge[0]].append(edge[1])
-                isolatedEdges[edge[1]] = isolatedEdges[edge[0]]
+                isolated_edges[edge[0]].append(edge[1])
+                isolated_edges[edge[1]] = isolated_edges[edge[0]]
 
         forest.append(edge)
-        connectedVertices.add(edge[0])
-        connectedVertices.add(edge[1])
+        connected_vertices.add(edge[0])
+        connected_vertices.add(edge[1])
 
     for i in range(size):
-        for j in G[i]:
-            if j not in isolatedEdges[i]:
+        for j in graph[i]:
+            if j not in isolated_edges[i]:
                 forest.append(sorted([i, j]))
-                branch = isolatedEdges[i]
-                isolatedEdges[i] += isolatedEdges[j]
-                isolatedEdges[j] += branch
+                branch = isolated_edges[i]
+                isolated_edges[i] += isolated_edges[j]
+                isolated_edges[j] += branch
 
     return forest
